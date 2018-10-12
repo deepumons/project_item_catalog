c9b43c5f8941c6474b3dd0ba0b2ccbc0b6364a97
Deepak Rajendran <urdeepak@gmail.com>
2018-10-12 12:47:19 +0000

    feat: Enabled template support for the /catalog route

--- project.py
+++ project.py
@@ -6,17 +6,17 @@ category = {'name': 'Soccer', 'id': '1'}
 categories = [{'name': 'Soccer', 'id': '1'}, {'name': 'Basketball', 'id': '2'},
 {'name': 'Baseball', 'id': '3'}, {'name': 'Frisbee', 'id': '4'},
 {'name': 'Snowboarding', 'id': '5'}]
-items = [{'id': '1', 'title': 'Goggles', 'description': 'Polarized goggles.', 'category_id': '5'},
-{'id': '2', 'title': 'Snowboard', 'description': 'Awesome Snowboard.', 'category_id': '5'},
-{'id': '3', 'title': 'Two Shinguards', 'description': 'Shinguards x 2.', 'category_id': '1'},
-{'id': '4', 'title': 'Shinguard', 'description': 'High quality shinguard', 'category_id': '1'},
-{'id': '5', 'title': 'Bat', 'description': 'High quality red wood bat', 'category_id': '3'}]
+items = [{'id': '1', 'name': 'Goggles', 'description': 'Polarized goggles.', 'category_id': '5', 'category_name': 'Snowboarding'},
+{'id': '2', 'name': 'Snowboard', 'description': 'Awesome Snowboard.', 'category_id': '5', 'category_name': 'Snowboarding'},
+{'id': '3', 'name': 'Two Shinguards', 'description': 'Shinguards x 2.', 'category_id': '1', 'category_name': 'Soccer'},
+{'id': '4', 'name': 'Shinguard', 'description': 'High quality shinguard', 'category_id': '1', 'category_name': 'Soccer'},
+{'id': '5', 'name': 'Bat', 'description': 'High quality red wood bat', 'category_id': '3', 'category_name': 'Baseball'}]
 
 
 @app.route("/")
 @app.route("/catalog")
 def list_catalog():
-    return render_template("catalog.html")
+    return render_template("catalog.html", categories=categories, items=items)
 
 
 @app.route("/catalog/<string:category_name>/")
