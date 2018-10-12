aea8db2e390a60bb34d3d5faea0ae0a4bd3071ec
Deepak Rajendran <urdeepak@gmail.com>
2018-10-12 12:11:56 +0000

    feat: Added tempory categories, and items for testing purposes

--- project.py
+++ project.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# dictionary for a single category, categories, and items for testing
+category = {'name': 'Soccer', 'id': '1'}
+categories = [{'name': 'Soccer', 'id': '1'}, {'name': 'Basketball', 'id': '2'},
+{'name': 'Baseball', 'id': '3'}, {'name': 'Frisbee', 'id': '4'},
+{'name': 'Snowboarding', 'id': '5'}]
+items = [{'id': '1', 'title': 'Goggles', 'description': 'Polarized goggles.', 'category_id': '5'},
+{'id': '2', 'title': 'Snowboard', 'description': 'Awesome Snowboard.', 'category_id': '5'},
+{'id': '3', 'title': 'Two Shinguards', 'description': 'Shinguards x 2.', 'category_id': '1'},
+{'id': '4', 'title': 'Shinguard', 'description': 'High quality shinguard', 'category_id': '1'},
+{'id': '5', 'title': 'Bat', 'description': 'High quality red wood bat', 'category_id': '3'}]
+
 
 @app.route("/")
 @app.route("/catalog")
-def list_catalog():    
+def list_catalog():
     return render_template("catalog.html")
 
 
