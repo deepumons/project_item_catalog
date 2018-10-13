1662ef55dcaa399666ab08dafd4c8af6762c0867
Deepak Rajendran <urdeepak@gmail.com>
2018-10-13 06:08:57 +0000

    chore: Removed comments and code cleanup

--- project.py
+++ project.py
@@ -28,7 +28,6 @@ def list_category_items(category_name):
 
 @app.route("/catalog/<string:category_name>/<string:item_name>")
 def list_item(category_name, item_name):
-    #return "Lists item discription for: {}".format(item_name)
     return render_template("item.html", categories=categories, item=items[0])
 
 
