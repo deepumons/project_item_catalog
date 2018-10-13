a146a5c45aded8f21e33e4dd8c855cf73254b366
Deepak Rajendran <urdeepak@gmail.com>
2018-10-13 06:03:31 +0000

    Added template support for /catalog/category/item route

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ category = {'name': 'Soccer', 'id': '1'}
 categories = [{'name': 'Soccer', 'id': '1'}, {'name': 'Basketball', 'id': '2'},
 {'name': 'Baseball', 'id': '3'}, {'name': 'Frisbee', 'id': '4'},
 {'name': 'Snowboarding', 'id': '5'}]
+item = {'id': '1', 'name': 'Goggles', 'description': 'Polarized goggles.', 'category_id': '5', 'category_name': 'Snowboarding'}
 items = [{'id': '1', 'name': 'Goggles', 'description': 'Polarized goggles.', 'category_id': '5', 'category_name': 'Snowboarding'},
 {'id': '2', 'name': 'Snowboard', 'description': 'Awesome Snowboard.', 'category_id': '5', 'category_name': 'Snowboarding'},
 {'id': '3', 'name': 'Two Shinguards', 'description': 'Shinguards x 2.', 'category_id': '1', 'category_name': 'Soccer'},
@@ -22,12 +23,13 @@ def list_catalog():
 @app.route("/catalog/<string:category_name>/")
 @app.route("/catalog/<string:category_name>/items")
 def list_category_items(category_name):
-    return render_template("items.html", category_name=category_name, items=items, categories=categories)    
+    return render_template("items.html", category_name=category_name, items=items, categories=categories)
 
 
 @app.route("/catalog/<string:category_name>/<string:item_name>")
 def list_item(category_name, item_name):
-    return "Lists item discription for: {}".format(item_name)
+    #return "Lists item discription for: {}".format(item_name)
+    return render_template("item.html", categories=categories, item=items[0])
 
 
 @app.route("/catalog/<string:category_name>/<string:item_name>/edit")
