7d971d8b07269fcdfe4c9012c9b57bbc331e1523
Deepak Rajendran <urdeepak@gmail.com>
2018-10-18 10:37:29 +0530

    feat: Enabled form submission for add_item method

--- project.py
+++ project.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, flash
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, CategoryItem
-
+from flask import session as login_session
+import datetime
 
 app = Flask(__name__)
 
@@ -11,15 +12,30 @@ engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
+
+# All the routes for the application are defined here
 @app.route("/")
 @app.route("/catalog/")
 def list_catalog():
+    # To be removed. Setting the session user_id for testing
+    login_session['user_id'] = 1
+    login_session['email'] = "urdeepak@gmail.com"
+    # del login_session['user_id']
+    # Test code ends
+
+    if 'user_id' not in login_session:
+        add_item = False
+    else:
+        add_item = True
+
     session = DBSession()
     categories = session.query(Category).all()
-    latest_items = session.query(CategoryItem).order_by(desc(CategoryItem.date))
+    latest_items = session.query(
+        CategoryItem).order_by(desc(CategoryItem.date))
     session.close()
     return render_template(
-        "catalog.html", categories=categories, items=latest_items)
+        "catalog.html", categories=categories, items=latest_items,
+        add_item=add_item)
 
 
 @app.route("/catalog/<string:category_name>/")
@@ -39,39 +55,87 @@ def list_category_items(category_name):
 def list_item(category_name, item_name):
     session = DBSession()
     categories = session.query(Category).all()
-    item = session.query(CategoryItem).filter_by(name = item_name).one()
-    category_name=item.category.name
+    item = session.query(CategoryItem).filter_by(name=item_name).one()
+    category_name = item.category.name
     session.close()
-    return render_template("item.html", categories=categories, item=item, category_name=category_name)
+    return render_template(
+        "item.html", categories=categories, item=item,
+        category_name=category_name)
 
 
 @app.route("/catalog/<string:category_name>/<string:item_name>/edit")
 def edit_item(category_name, item_name):
     session = DBSession()
     categories = session.query(Category).all()
-    item = session.query(CategoryItem).filter_by(name = item_name).one()    
+    item = session.query(CategoryItem).filter_by(name=item_name).one()
     session.close()
     return render_template(
-        "edit_item.html", categories=categories, item=item, category_name=category_name)
+        "edit_item.html", categories=categories, item=item,
+        category_name=category_name)
 
 
 @app.route("/catalog/<string:category_name>/<string:item_name>/delete")
 def delete_item(category_name, item_name):
     session = DBSession()
     categories = session.query(Category).all()
-    item = session.query(CategoryItem).filter_by(name = item_name).one()
+    item = session.query(CategoryItem).filter_by(name=item_name).one()
     session.close()
     return render_template(
         "delete_item.html", categories=categories, item=item)
 
 
-@app.route("/catalog/add")
+@app.route("/catalog/add", methods=['GET', 'POST'])
 def add_item():
+    # Check if the user is logged in before going further
+    if 'user_id' not in login_session:
+        flash("You should login before you can create a catalog item.")
+        return redirect(url_for('list_catalog'))
+
     session = DBSession()
     categories = session.query(Category).all()
     session.close()
-    return render_template(
-        "add_item.html", categories=categories)
+
+    if request.method == 'POST':
+        # If the user has pressed the cancel button, abort and go back
+        if request.form['submit_button'] == 'cancel':
+            return redirect(url_for('list_catalog'))
+        else:
+            # See if the user has entered any data before proceeding
+            if request.form['name'] is None or request.form['name'] == '':
+                flash("Please enter the name of the catalog item before
+                      submiting the form.")
+                return render_template(
+                    "add_item.html", categories=categories)
+            else:
+                user_id = getUserID(login_session['email'])
+
+                session = DBSession()
+                category = session.query(Category).filter_by(
+                            name=request.form['category']).one()
+
+                newItem = CategoryItem(
+                    name=request.form['name'],
+                    description=request.form['description'],
+                    date=datetime.datetime.now(),
+                    category=category,
+                    user_id=user_id)
+
+                session.add(newItem)
+                session.commit()
+                session.close()
+
+                flash("New item added successfully.")
+                return redirect(url_for('list_catalog'))
+    else:
+        return render_template(
+            "add_item.html", categories=categories)
+
+
+def getUserID(email):
+        session = DBSession()
+        user = session.query(User).filter_by(email=email).one()
+        session.close()
+        return user.id
 
 
 if __name__ == "__main__":
